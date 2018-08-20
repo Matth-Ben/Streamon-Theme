a839b806b03156c5772760c2e600a1633058ce7a
Matth-Ben <matthias.benoit0702@gmail.com>
2018-08-20 17:51:58 +0200

    Update template Homepage

--- tpl-homepage.php
+++ tpl-homepage.php
@@ -9,4 +9,26 @@
 /*
 Template Name: Accueil
 */
+?>
 
+<?php get_header(); ?>
+
+<div class="container-fullwidth">
+    <div class="container">
+        <div class="page-header">
+            <h1 class="archive_title h2">
+                <?php the_title(); ?>
+            </h1>
+            <hr class="title_highlight">
+        </div>
+    </div>
+
+<?php
+
+the_content();
+
+?>
+
+</div>
+
+<?php get_footer(); ?>
